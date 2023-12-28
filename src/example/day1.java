92a27e4637bd368fd7d3b00b07b873bac8fb76a0
류광희 <kwang9269@gmail.com>
2023-12-28 18:50:02 +0900

    코딩테스트1

--- src/example/day1.java
+++ src/example/day1.java
@@ -1,10 +1,32 @@
 package example;
 
+import java.util.Scanner;
+
 public class day1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	    public static void main(String[] args){
+	        Scanner sc = new Scanner(System.in);
+	        int a = sc.nextInt();
+	        
+	        char grade;
+	       switch (a / 10) {
+			case 10: 
+			case 9:
+				grade = 'A';
+				break;
+			case 8:
+				grade = 'B';
+				break;
+			case 7:
+				grade = 'C';
+				break;
+			case 6:
+				grade = 'D';
+				break;
+			default:
+				grade = 'F';
+				break;
+				
+	    }
+	       System.out.println(grade);
+	    }
 }
---
+++ src/example/day2.java
@@ -0,0 +1,40 @@
+package example;
+
+import java.util.Scanner;
+
+public class day2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Scanner sc = new Scanner(System.in);
+		
+		
+		 * int n = sc.nextInt(); 
+		 * if(n >=1 && n <10) { for (int i = 1; i < 10; i++) {
+		 * System.out.println(n + " * " + i + " = " + i*n); } }
+		 */
+		
+		/*Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		if( A > B) {
+			System.out.println(">");
+		} else if( A < B ) {
+			System.out.println("<");
+		} else
+			System.out.println("==");
+			*/
+		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i = 1; i < N+1; i++) {
+			for(int a = 1; a < i+1; a++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+		
+
+}
